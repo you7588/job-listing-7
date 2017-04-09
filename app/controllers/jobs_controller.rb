c1d53b7098e5258285c3aaba0a424b517ad28bf3
you7588 <yo7588@gmail.com>
2017-04-09 18:42:58 +0800

    implement jobs#destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -19,6 +19,10 @@ class JobsController < ApplicationController
     redirect_to jobs_path
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def update
     @job = Job.find(params[:id])
 
@@ -27,9 +31,14 @@ class JobsController < ApplicationController
     redirect_to jobs_path, notice: "Update Success"
   end
 
-  def edit
+  def destroy
     @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job deleted"
+    redirect_to jobs_path
   end
+
+
 
   private
 
