3e66bf21ba33347f211ce92ee315f48df75f9e95
you7588 <yo7588@gmail.com>
2017-04-09 18:55:00 +0800

    add validation to title

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,9 +14,12 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
-
-    redirect_to jobs_path
+    
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
   def edit
