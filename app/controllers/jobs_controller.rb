fe32aa02c000dd9b1a02737bbb1d2928e0ff5bcf
you7588 <yo7588@gmail.com>
2017-04-10 19:04:54 +0800

    Check the wrong thing

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -30,9 +30,9 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path, notice: "Update Success"
+      redirect_to jobs_path
     else
-      render  :edit
+      render :edit
     end
   end
 
